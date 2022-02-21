d2dee88abe28893290219537989590b1943eb7e7
VolAndRnD <volk0096@mail.ru>
2022-02-21 16:49:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        boolean isGreenLight = false;
+
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+
+        int droppedOut = 0;
+        if (isGreenLight) {
+            System.out.println("Выбывает " + droppedOut); // если зеленый свет
+        }
+        else {
+              if (speedOfPlayer1 != 0) {
+                droppedOut += 1;
+              }
+              else {
+                droppedOut += 0;
+              } // Первый участник
+              if (speedOfPlayer2 != 0) {
+                droppedOut += 1;
+              }
+              else {
+                droppedOut += 0;
+              } // Второй участник
+              if (speedOfPlayer3 != 0) {
+                droppedOut += 1;
+              }
+              else {
+                droppedOut += 0;
+              } // Третий участник
+            System.out.println("Выбывает " + droppedOut); // если красный свет
+        }
+
+    }
+}
+        // Допишите здесь логику так, что будет подсчитано и выведено
+        // количество игроков, которые выбывают.
+        // Если свет зелёный, то проходят все игроки (0 выбывают).
+        // Если свет красный, то выбывает каждый, чья скорость не 0.
+
+
