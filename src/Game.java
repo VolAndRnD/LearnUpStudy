c17a008deca001deec785a562d4251eaba95bf03
VolAndRnD <volk0096@mail.ru>
2022-03-11 18:53:27 +0300

    Update 2.0.0

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,14 @@
 public class Game {
-    public static void player1 (int a) {
-        System.out.println("Игрок 1 прошел? - " + (a == 0));
-    }
-    public static void player2 (int b) {
-        System.out.println("Игрок 2 прошел? - " + (b == 0));
-    }
-    public static void player3 (int c) {
-        System.out.println("Игрок 3 прошел? - " + (c == 0));
-    }
-    public static int sumDroppedOut (int droppedOut1, int droppedOut2, int droppedOut3) {
-        int sum = droppedOut1 + droppedOut2 + droppedOut3;
-        return sum;
-    }
+
+    static boolean isGreenLight = false;
 
 
+    public static boolean player () {
+        boolean player = true;
+        if (player == isGreenLight) { player = false;}
+        else { player = (Main.speed()!= 0);}
+            return player;
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,16 @@
 class Main {
     public static void main(String[] args) {
-
-        boolean isGreenLight = false;
-        int speedOfPlayer1 = 5;
-        int speedOfPlayer2 = 0;
-        int speedOfPlayer3 = 2;
-
-        int droppedOut1 = 0;
-        int droppedOut2 = 0;
-        int droppedOut3 = 0;
-
-        if (isGreenLight) {
-            System.out.println("Выбывает " + Game.sumDroppedOut(droppedOut1,droppedOut2,droppedOut3)); // если зеленый свет
-        } else {
-            if (speedOfPlayer1 != 0) {
-                droppedOut1 ++;
-            }// Первый участник
-            if (speedOfPlayer2 != 0) {
-                droppedOut2 ++;
-            }// Второй участник
-            if (speedOfPlayer3 != 0) {
-                droppedOut3 ++;
-            }// Третий участник
-            System.out.println("Выбывает " + Game.sumDroppedOut(droppedOut1,droppedOut2,droppedOut3)); // если красный свет
-
-        }
-        Game.player1(droppedOut1);
-        Game.player2(droppedOut2);
-        Game.player3(droppedOut3);
+        boolean remain = Game.player();
+        System.out.println( "Игрок выбывает?" + "-" + remain );
+    }
+    public static int speed () {
+        int speed = 5;
+        return speed;
     }
 }
-        // Допишите здесь логику так, что будет подсчитано и выведено
-        // количество игроков, которые выбывают.
-        // Если свет зелёный, то проходят все игроки (0 выбывают).
-        // Если свет красный, то выбывает каждый, чья скорость не 0.
+
+
+
+
 
 
