a3805be1221ece589d6c040fe31a0f967412fabe
VolAndRnD <volk0096@mail.ru>
2022-03-12 16:32:32 +0300

    Update 2.1.0

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,9 @@
 public class Game {
 
-    static boolean isGreenLight = false;
+    static boolean isGreenLight = true;
 
-
-    public static boolean player () {
-        boolean player = true;
-        if (player == isGreenLight) { player = false;}
-        else { player = (Main.speed()!= 0);}
-            return player;
+    public static boolean player (int speed) {
+        if (isGreenLight) { return false;}
+        else { return speed != 0;}
     }
 }
-
-
-
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 class Main {
     public static void main(String[] args) {
-        boolean remain = Game.player();
-        System.out.println( "Игрок выбывает?" + "-" + remain );
-    }
-    public static int speed () {
-        int speed = 5;
-        return speed;
+
+        Game.isGreenLight = true;
+        System.out.println( "Игрок со скоростью 2 выбывает? - " +  Game.player(2) );
+        System.out.println( "Игрок со скоростью 4 выбывает? - " +  Game.player(4) );
+        System.out.println( "Игрок со скоростью 0 выбывает? - " +  Game.player(0) );
+
+        Game.isGreenLight = false;
+        System.out.println( "Игрок со скоростью 1 выбывает? - " +  Game.player(1) );
+        System.out.println( "Игрок со скоростью 0 выбывает? - " +  Game.player(0) );
+        System.out.println( "Игрок со скоростью 9 выбывает? - " +  Game.player(9) );
     }
 }
 
